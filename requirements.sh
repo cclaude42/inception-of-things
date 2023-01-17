02af905715bf0a4e6366fb90092eadd4f903df42
Cesar Claude <cesarclaudepro@gmail.com>
2023-01-17 14:17:27 +0100

    :construction: still working on install script

--- requirements.sh
+++ requirements.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Install Vagrant
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install -y vagrant-2.3.4
+# Prerequisites
+sudo apt install -y curl
 
 # Install Virtualbox
-wget -P /https://download.virtualbox.org/virtualbox/7.0.4/virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64.deb
+wget -P /tmp https://download.virtualbox.org/virtualbox/7.0.4/virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64>
 sudo apt install -y /tmp/virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64.deb
 
-# Install Docker
-##
+# Install Vagrant
+wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y vagrant=2.3.4
 
-# Install kubectl ?
-##
+# Install Docker
+curl -sSL https://get.docker.com/ | VERSION=20.10.22 sh
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/v1.26.0/bin/linux/amd64/kubectl.sha256"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Install k3d
-sudo apt install -y curl
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.4.6 bash
